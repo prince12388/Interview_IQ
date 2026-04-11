bb39d070e075b8bf6eeff85639e2edcb18ce9155
Prince Yadav <yadavprince70005@gmail.com>
2026-04-11 20:27:13 +0530

    abcd

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain: "interviewiq-e714d.firebaseapp.com",
-    projectId: "interviewiq-e714d",
-    storageBucket: "interviewiq-e714d.firebasestorage.app",
-    messagingSenderId: "107382980162",
-    appId: "1:107382980162:web:2ae0d3fc3f94afa9a0f7db"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "interviewiq-e714d.firebaseapp.com",
+  projectId: "interviewiq-e714d",
+  storageBucket: "interviewiq-e714d.firebasestorage.app",
+  messagingSenderId: "107382980162",
+  appId: "1:107382980162:web:2ae0d3fc3f94afa9a0f7db"
 };
 
 const app = initializeApp(firebaseConfig);
